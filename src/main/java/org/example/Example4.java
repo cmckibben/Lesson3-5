532e2e6a82d27bc5eca01c51bb275ed54d926e22
cmckibben <christopher_mckibben@hcpss.org>
2024-10-04 09:05:18 -0400

    fixed error

--- src/main/java/org/example/Example4.java
+++ src/main/java/org/example/Example4.java
@@ -3,9 +3,8 @@ package org.example;
 public class Example4 {
     public static void main(String[] args) {
         int x = 5;
-        System.out.print((true || (x/0)==1));
+        System.out.println((true || (x/0)==1));
 
-        x = 5;
-        System.out.print((false && (x/0)==1));
+        System.out.println((false && (x/0)==1));
     }
 }
